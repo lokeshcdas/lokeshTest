e7f614c941336c1e71c194df1e74fa10f446ce4d
Lokesh Das <lokesh.das@bjitgroup.com>
2016-05-03 16:56:38 +0600

    prime number

---
+++ test.py
@@ -0,0 +1,12 @@
+def printPrime(range1, range2):
+    flag = 0
+    for num in range(range1, range2):
+        for i in range(2, num/2):
+            if(num%i==0):
+                flag= 1
+                break
+            else:
+                flag=0
+        if(flag==0):
+            print(num)
+printPrime(100,200)
